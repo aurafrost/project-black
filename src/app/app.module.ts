b3782bc186e34a6bce766d4bc3c29f206b387fcc
jimmytran <jt963369@gmail.com>
2019-01-14 09:56:23 -0500

    Header/footer update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { SupportComponent } from './support/support.component';
+import { AccountComponent } from './account/account.component';
+import { TermsComponent } from './terms/terms.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    SupportComponent,
+    AccountComponent,
+    TermsComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
