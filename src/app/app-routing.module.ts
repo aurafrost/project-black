f67ae6ba5229c83aad80f4cef72420573a403ebb
SocialAppBeta111 <fadjovu@hotmail.com>
2019-01-14 13:30:04 -0500

    component changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { AccountComponent } from './pages/account/account.component';
 import { ContentComponent } from './pages/content/content.component';
-import { RegisterComponent } from './register/register.component';
-import { FaqComponent } from './faq/faq.component';
-import { CustomersupportComponent } from './customersupport/customersupport.component';
-import { PrivacyComponent } from './privacy/privacy.component';
+
+import { FaqComponent } from './pages/faq/faq.component';
 import { ErrComponent } from './err/err.component';
 
 const routes: Routes = [
@@ -14,10 +12,7 @@ const routes: Routes = [
 {path: 'about', component: AboutComponent},
 {path: 'account', component: AccountComponent},
 {path: 'content', component: ContentComponent},
-{path: 'register', component: RegisterComponent},
 {path: 'faq', component: FaqComponent},
-{path: 'customersupport', component: CustomersupportComponent},
-{path: 'privacy', component: PrivacyComponent},
 {path: '**', component: ErrComponent}
 ];
 
