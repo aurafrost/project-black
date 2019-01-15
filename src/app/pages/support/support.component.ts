310bf68c207c2018e750b652391d3caa38249d2f
Jacob Crumbley <jicrumbley@gmail.com>
2019-01-15 13:23:39 -0500

    finalized support form

--- src/app/pages/support/support.component.ts
+++ src/app/pages/support/support.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'support',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./support.component.css']
 })
 export class SupportComponent implements OnInit {
-
+  formData;
+  supportRequest;
   constructor() { }
 
   ngOnInit() {
+    this.formData = new FormGroup({
+      email: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      message: new FormControl('', Validators.compose([
+        Validators.required
+      ]))
+    })
+  }
+
+  postSupport() {
+    this.supportRequest = this.formData.value;
+    this.ngOnInit();
   }
 
 }
