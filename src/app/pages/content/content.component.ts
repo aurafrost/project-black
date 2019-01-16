10b88f4235b20c5c5e693ee026e68019bd6e8e8e
jimmytran <jt963369@gmail.com>
2019-01-16 11:21:17 -0500

    content fix

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -13,6 +13,10 @@ export class ContentComponent implements OnInit {
     //get all subscribed posts
   }
 
+  subscribe(){
+    this.hide;
+  }
+  
   hide(id){
     this.htmlele=document.getElementById(id) as HTMLElement;
     this.htmlele.parentNode.removeChild(this.htmlele);
