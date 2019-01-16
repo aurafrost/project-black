4838efbc916fc74c486c32ca742fdc7979cd6e09
Zack Blaylock <zackblaylock@gmail.com>
2019-01-16 13:09:10 -0500

    subscribe btn on explore page

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'explore',
@@ -17,7 +19,15 @@ export class ExploreComponent implements OnInit {
     {name:'Willem Dafoe', numSubs: 12500, postsMonth: 12, img: "../../../assets/png/Willem_Dafoe_200x200.png"}
   ]
 
-  constructor() { }
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon('people_outline',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/svg/sharp-people_outline-24px.svg'));
+    this.matIconRegistry.addSvgIcon('av_timer',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/svg/outline-av_timer-24px.svg'));
+  }
 
   ngOnInit() {
   }
