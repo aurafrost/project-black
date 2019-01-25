22d216e2a4840aa1d4333c663d252ed52227a78f
Zack Blaylock <zackblaylock@gmail.com>
2019-01-25 11:25:55 -0500

    Added Shopping cart branch

---
+++ src/app/pages/shopping-cart/test-shop/test-shop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'test-shop',
+  templateUrl: './test-shop.component.html',
+  styleUrls: ['./test-shop.component.css']
+})
+export class TestShopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
