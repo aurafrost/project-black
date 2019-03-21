498d1cd8e94c6dd5f16e0b8c3bd5e9122f6de3cc
jimmytran <jt963369@gmail.com>
2019-03-21 10:38:45 -0400

    prod changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/project-black-personal'));
+app.use(express.static('./dist/project-black'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/project-black-personal/index.html'));
+res.sendFile(path.join(__dirname,'/dist/project-black/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
